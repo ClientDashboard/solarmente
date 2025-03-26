3313bce0ccfc984b0a30fb910397b37b92ff43e0
Alvaro Lopez <alvarolopez@Alvaros-Mac-mini.local>
2025-03-26 11:21:04 -0500

    Configurar Next.js para ignorar errores de TypeScript durante el build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  typescript: {
+    // Ignora errores de TypeScript durante la compilación
+    ignoreBuildErrors: true,
+  },
 };
 
 module.exports = nextConfig;
